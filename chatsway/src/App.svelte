70c6342ea6bd662f18f63718d1a2b8a1d6a1c385
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-04-07 16:19:59 +0400

    initialized svelte project

---
+++ chatsway/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<h1 class="title">ChatSway - Seamless AI Chat</h1>
+
+<style>
+  .title {
+    font-size: 2rem;
+    font-weight: 700;
+    color: #333;
+    margin: 1rem 0;
+    text-align: center;
+  }
+</style>
