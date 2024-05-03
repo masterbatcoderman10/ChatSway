0c9422d61982a024d16eee010dac42cebf6d30ca
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-05-03 21:06:51 +0400

    added timeout/wait functionality

--- api/main.py
+++ api/main.py
@@ -13,7 +13,14 @@ async def chat(websocket: WebSocket):
     print("Client connected")
     try:
         while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
+                try:
+                    print("Waiting for message...")
+                    data = await asyncio.wait_for(websocket.receive_text(), timeout=5)
+                    await websocket.send_text(f"Message text was: {data}")
+                except asyncio.TimeoutError:
+                    # await websocket.send_text("Timeout!")
+                    # print("Timeout!")
+                    pass
+            #actually will process response.
     except WebSocketDisconnect:
         print("Client disconnected")
